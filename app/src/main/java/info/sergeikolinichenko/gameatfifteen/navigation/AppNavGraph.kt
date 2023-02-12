215088bbc7a1eff1110730adf550c8caaf8393d4
Sergei Kolinichenko <iamserj61@gmail.com>
2023-02-12 19:40:11 +0200

    added animation appear screen GameScore

--- app/src/main/java/info/sergeikolinichenko/gameatfifteen/navigation/AppNavGraph.kt
+++ app/src/main/java/info/sergeikolinichenko/gameatfifteen/navigation/AppNavGraph.kt
@@ -1,26 +1,62 @@
 package info.sergeikolinichenko.gameatfifteen.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 
 /** Created by Sergei Kolinichenko on 24.01.2023 at 19:01 (GMT+3) **/
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AppNavGraph(
     navHostController: NavHostController,
     gameScreenContent: @Composable () -> Unit,
     scoresScreenContent: @Composable () -> Unit
 ) {
-    NavHost(
+    AnimatedNavHost(
         navController = navHostController,
         startDestination = Screen.Game.route
     ) {
-        composable(route = Screen.Game.route) {
+        composable(
+            route = Screen.Game.route,
+            exitTransition = {
+                slideOutHorizontally(targetOffsetX = { -1000 },
+                    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+                )
+            },
+            popEnterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { 1000 },
+                    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+                )
+            },
+            popExitTransition = {
+                slideOutHorizontally(targetOffsetX = { -1000 },
+                    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy))
+            }
+
+        ) {
             gameScreenContent()
         }
-        composable(route = Screen.Scores.route) {
+        composable(route = Screen.Scores.route,
+            enterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { 1000 },
+                    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+                )
+            },
+            exitTransition = {
+                slideOutHorizontally(targetOffsetX = { -1000 },
+                    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+                )
+            }
+        ) {
             scoresScreenContent()
         }
     }
--- app/src/main/java/info/sergeikolinichenko/gameatfifteen/screens/GameMainScreen.kt
+++ app/src/main/java/info/sergeikolinichenko/gameatfifteen/screens/GameMainScreen.kt
@@ -1,7 +1,8 @@
 package info.sergeikolinichenko.gameatfifteen.screens
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import info.sergeikolinichenko.gameatfifteen.navigation.AppNavGraph
 import info.sergeikolinichenko.gameatfifteen.navigation.Screen
 import info.sergeikolinichenko.gameatfifteen.screens.game.compose.GameScreen
@@ -11,13 +12,14 @@ import info.sergeikolinichenko.gameatfifteen.screens.score.logic.ScoreViewModel
 
 /** Created by Sergei Kolinichenko on 24.01.2023 at 19:52 (GMT+3) **/
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun GameMainScreen(
     gameViewModel: GameViewModel,
     scoreViewModel: ScoreViewModel
 ) {
 
-    val navHostController = rememberNavController()
+    val navHostController = rememberAnimatedNavController()
     
     AppNavGraph(
         navHostController = navHostController,
